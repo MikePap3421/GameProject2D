8ddaa9799e149fd0662069906550baa0b0396ec5
MikePap3421 <micelpap4@gmail.com>
2024-12-01 20:22:58 +0200

    Add files via upload

--- src/managers/WaveManager.java
+++ src/managers/WaveManager.java
@@ -47,8 +47,8 @@ public class WaveManager {
     }
 
     private void createWaves() {
-       // waves.add(new Wave(new ArrayList<Integer>(Arrays.asList(0,0,0))));
-        waves.add(new Wave(new ArrayList<Integer>(Arrays.asList(0,0,1,0,2))));
+        waves.add(new Wave(new ArrayList<Integer>(Arrays.asList(0,0,0))));
+        //waves.add(new Wave(new ArrayList<Integer>(Arrays.asList(0,0,1,0,2))));
        // waves.add(new Wave(new ArrayList<Integer>(Arrays.asList(0,1,2,1,0,0,2,1,2))));
        // waves.add(new Wave(new ArrayList<Integer>(Arrays.asList(1,1,2,1,0,1,2,1,2,0))));
        // waves.add(new Wave(new ArrayList<Integer>(Arrays.asList(2,1,2,0,1,1,1,0,1,1,0,2,1,1))));
